dd81ed5fc158219e9878f3cb126d42b0942ab05f
Srijon Saha <srijon365@gmail.com>
2019-10-16 23:34:24 -0900

    Add create-react-app code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Sync Along</h1>
+    </div>
+  );
+}
+
+export default App;
