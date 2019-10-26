3c659dfe09d5db4e71711e38b2e5d38fda055f7f
Lorenzo Bisceglia <lorenzo.bisceglia9@gmail.com>
2019-10-25 22:17:57 -0700

    added embedded YouTube player to main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Sync Along</h1>
+      <YouTube videoId={'HXcSGuYUkDg'}/>
     </div>
   );
 }
