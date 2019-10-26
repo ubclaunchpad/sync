ef51d3107b0d05db04fb2f33abaf8e5ea168ab77
klynx <KevinLynx_L@hotmail.com>
2019-10-26 13:53:55 -0700

    Convert db into separate helper file

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,31 @@
-var redis = require("redis"),
-    client = redis.createClient();
- 
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
- 
-client.on('connect', () => {
-    console.log(`connected to redis`);
-});
-client.on('error', err => {
-    console.log(`Error: ${err}`);
-});
- 
-// client.set("string key", "string val", redis.print);
-// client.hset("hash key", "hashtest 1", "some value", redis.print);
-// client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-// client.hkeys("hash key", function (err, replies) {
-//     console.log(replies.length + " replies:");
-//     replies.forEach(function (reply, i) {
-//         console.log("    " + i + ": " + reply);
-//     });
-//     client.quit();
-// });
+class DBS{
+    constructor(){
+        this.port = 6379;
+        this.host = "127.0.0.1";
+        this.redis = require('redis');
+        this.client = this.redis.createClient(this.port, this.host);
+
+        this.client.on('connect', () => {
+            console.log(`connected to redis`);
+        });
+        this.client.on('error', err => {
+            console.log(`Error: ${err}`);
+        });
+    }
+
+    // Set up Queries here
+    testOutput(){
+        // Example
+        this.client.set('my test key', {foo: "bar"}, this.redis.print);
+        this.client.get('my test key', function (error, result) {
+            if (error) {
+                console.log(error);
+                throw error;
+            }
+            console.log('GET result ->' + result);
+        });
+    }
+}
+
+
+module.exports = DBS;
