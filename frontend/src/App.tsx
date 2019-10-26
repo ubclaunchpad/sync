57bba929b0afcd720804c397fa00fdb2691a72ea
Lorenzo Bisceglia <lorenzo.bisceglia9@gmail.com>
2019-10-25 22:44:23 -0700

    added router and linked to Rooms page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import YouTube from 'react-youtube';
+import Rooms from './pages/rooms';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Sync Along</h1>
-      <YouTube videoId={'HXcSGuYUkDg'}/>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import Rooms from './pages/rooms';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/rooms">Rooms</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/rooms" component={Rooms} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
