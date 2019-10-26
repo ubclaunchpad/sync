beb3fcaec4b389004c8129db545962688e05cc4b
John Aguilar <42220961+johnagl@users.noreply.github.com>
2019-10-26 13:58:55 -0700

    Removed types from server, allow implicit typing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
-import http, { Server } from 'http';
-import express, { Application } from 'express';
+import http from 'http';
+import express  from 'express';
 import bodyParser from 'body-parser';
 import socketIo from 'socket.io';
 import router from './routes';
 import  { ServerEvent }  from './constants';
 
-const PORT: string | number = process.env.PORT || 8080; //@type?
-const app: Application = express();
-const server: Server = http.createServer(app);
+const PORT = process.env.PORT || 8080; 
+const app = express();
+const server = http.createServer(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,13 +17,13 @@ server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-const io: SocketIO.Server = socketIo(server);
-io.on(ServerEvent.CONNECT, (socket: any) => {
+const io = socketIo(server);
+io.on(ServerEvent.CONNECT, (socket) => {
   console.log('User connected');
   
   socket.emit(ServerEvent.EVENT_FROM_SERVER_TEST, {data: "Welcome to the socketio server"});
 
-  socket.on(ServerEvent.EVENT_TO_SERVER_TEST, (dataFromClient: any) => {
+  socket.on(ServerEvent.EVENT_TO_SERVER_TEST, (dataFromClient) => {
     console.log('Event from client received'+ dataFromClient);
     io.emit(ServerEvent.EVENT_TO_ALL_TEST, 'io.emit to all test');
   });
@@ -32,7 +32,7 @@ io.on(ServerEvent.CONNECT, (socket: any) => {
   socket.on(ServerEvent.PLAY, () => {
     io.emit(ServerEvent.PLAY_ALL, 'Play');
   });
-  
+
   //TODO: When a client pauses, send a pause command to all clients
   socket.on(ServerEvent.PAUSE, () => {
     io.emit(ServerEvent.PAUSE_ALL, 'Pause');
