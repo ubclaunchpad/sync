11f788547a4a770e7330e8ffe6b76f3c2585824f
klynx <KevinLynx_L@hotmail.com>
2019-10-28 16:17:50 -0700

    Changed redis path to redis from localhost

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,4 +10,12 @@ router.get('/', (req, res) => {
   res.send('Hello world');
 })
 
+router.get('/findRooms/:roomid', (req, res) => {
+  redisDB.getRoom(req.params.roomid);
+})
+
+router.post('/createRoom', (req, res) => {
+  redisDB.createRoom(req.params.roomid, req.params.roominfo);
+})
+
 export default router;
