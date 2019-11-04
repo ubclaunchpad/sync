f750494bba405d1762e388a654e422bddab29227
John Aguilar <42220961+johnagl@users.noreply.github.com>
2019-11-03 22:16:37 -0800

    Install axios, add  roomId input box, handleSubmit, handleOnChange handlers, and getRoomId axios.get with hard coded room id

--- frontend/src/components/JoinPage.tsx
+++ frontend/src/components/JoinPage.tsx
@@ -1,15 +1,39 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Join extends Component {
-  componentDidMount(){
-    console.log('Entered join rooms page');
+  state = {
+    roomId: '',
   }
 
+  componentDidMount(){
+  }
+
+  getRoomId = () => {
+    axios
+      .get("http://localhost:8080/rooms?roomid=bee3c2f0-fec9-11e9-929b-cf04f489342f")
+      .then(data => console.log('DATA: ' + JSON.stringify(data)))
+      .catch(err => {
+        console.log(err);
+        return null;
+      })
+  };
+
+  handleSubmit = () => {
+    console.log(this.state.roomId);
+    this.getRoomId();
+  }
+
+  handleOnChange = (e: any) => {
+    this.setState({roomId: e.target.value });
+  }
 
   render() {
     return (
     <div>
       <h1>Join Page</h1>
+        Room id: <input onChange={this.handleOnChange} type="text" name="roomid"></input>
+        <button onClick={this.handleSubmit} >Submit</button>
     </div>);
   }
 }
