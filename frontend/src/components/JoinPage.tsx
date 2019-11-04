06c1ee4411b8a5524ea91cd2c25e2df7a95b9989
John Aguilar <42220961+johnagl@users.noreply.github.com>
2019-11-03 16:45:23 -0800

    Create /join route

---
+++ frontend/src/components/JoinPage.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Join extends Component {
+  componentDidMount(){
+    console.log('Entered join rooms page');
+  }
+
+
+  render() {
+    return (
+    <div>
+      <h1>Join Page</h1>
+    </div>);
+  }
+}
+
+export default Join;
