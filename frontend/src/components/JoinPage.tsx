551eacbe46112379417d29bf23b794c9d1eaa1c6
John Aguilar <42220961+johnagl@users.noreply.github.com>
2019-11-04 21:27:55 -0800

    Update getRoomId function and implement redirect if roomId exists

--- frontend/src/components/JoinPage.tsx
+++ frontend/src/components/JoinPage.tsx
@@ -1,18 +1,33 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 class Join extends Component {
   state = {
     roomId: '',
+    redirect: false,
   }
 
-  componentDidMount(){
+  componentDidMount(){}
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
   }
 
-  getRoomId = () => {
+  renderRedirect = () => {
+    const { roomId, redirect } = this.state;
+    const route = '/rooms' + '?roomid=' + roomId;
+    if (redirect) {
+      return <Redirect to={route} />
+    }
+  }
+
+  getRoomId = (roomId : string) => {
     axios
-      .get("http://localhost:8080/rooms?roomid=bee3c2f0-fec9-11e9-929b-cf04f489342f")
-      .then(data => console.log('DATA: ' + JSON.stringify(data)))
+      .get("http://localhost:8080/rooms?roomid=" + roomId)
+      .then(data => data.data ? this.setRedirect() : alert('Enter a valid room id'))
       .catch(err => {
         console.log(err);
         return null;
@@ -20,8 +35,8 @@ class Join extends Component {
   };
 
   handleSubmit = () => {
-    console.log(this.state.roomId);
-    this.getRoomId();
+    const { roomId } = this.state;
+    this.getRoomId(roomId);
   }
 
   handleOnChange = (e: any) => {
@@ -31,6 +46,7 @@ class Join extends Component {
   render() {
     return (
     <div>
+      {this.renderRedirect()}
       <h1>Join Page</h1>
         Room id: <input onChange={this.handleOnChange} type="text" name="roomid"></input>
         <button onClick={this.handleSubmit} >Submit</button>
