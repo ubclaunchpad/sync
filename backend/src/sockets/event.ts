72d9ff379e5886da6afcc07cc1d201a734fe6a20
Srijon Saha <srijon365@gmail.com>
2020-01-15 10:52:46 -0800

    Refactor socket event handling logic

---
+++ backend/src/sockets/event.ts
@@ -0,0 +1,8 @@
+export enum Event {
+  CONNECT = "connect",
+  DISCONNECT = "disconnect",
+  MESSAGE = "message",
+  JOIN_ROOM = "join room",
+  PLAY_VIDEO = "play video",
+  PAUSE_VIDEO = "pause video"
+}
