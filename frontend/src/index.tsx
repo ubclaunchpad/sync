74090c1691c4ec22713fefa2bf59d261d2890cca
Srijon Saha <srijon365@gmail.com>
2020-01-18 00:38:04 -0800

    Remove service worker file

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,8 +5,7 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import RoomsPage from './components/RoomsPage';
 import HomePage from './components/HomePage';
 import JoinPage from './components/JoinPage';
-import Room from './components/Room'
-import * as serviceWorker from './serviceWorker';
+import Room from './components/Room';
 
 const routing = (
   <Router>
@@ -32,8 +31,3 @@ const routing = (
 )
 
 ReactDOM.render(routing, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
