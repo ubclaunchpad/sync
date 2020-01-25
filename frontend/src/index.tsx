b0666bbd125c293e32d180d8124c85d00d38f412
Srijon Saha <srijon365@gmail.com>
2020-01-25 12:39:39 -0800

    Remove unneeded routes from index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-import RoomsPage from './components/RoomsPage';
-import HomePage from './components/HomePage';
-import JoinPage from './components/JoinPage';
 import Room from './components/Room';
 import Home from "./components/Home";
 import Browse from "./components/Browse";
+import './styles/index.css';
 
 const App = (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
-      <Route path="/join" component={JoinPage} />
-      <Route path="/room" component={Room} />
-      <Route path="/rooms" component={Browse} />
+      <Route exact path="/rooms/:id" component={Room} />
+      <Route exact path="/rooms" component={Browse} />
     </div>
   </Router>
 );
