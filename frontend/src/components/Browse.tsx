070efaf8de9367c9eb107f5d46dea0bd2ba35847
Srijon Saha <srijon365@gmail.com>
2020-01-25 01:48:26 -0800

    Rename discover to browse

---
+++ frontend/src/components/Browse.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Browse extends React.Component {
+
+  render() {
+    return (
+      <h1>Browse Rooms</h1>
+    );
+  }
+
+}
+
+export default Browse;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,14 +7,15 @@ import HomePage from './components/HomePage';
 import JoinPage from './components/JoinPage';
 import Room from './components/Room';
 import Home from "./components/Home";
+import Browse from "./components/Browse";
 
 const App = (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
-      <Route path="/rooms" component={RoomsPage} />
       <Route path="/join" component={JoinPage} />
       <Route path="/room" component={Room} />
+      <Route path="/rooms" component={Browse} />
     </div>
   </Router>
 );
