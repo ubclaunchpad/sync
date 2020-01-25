5d981c4890ebf61e49af63200391babb337d21cc
Srijon Saha <srijon365@gmail.com>
2020-01-25 01:37:09 -0800

    Move to new home component and update image names

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import RoomsPage from './components/RoomsPage';
 import HomePage from './components/HomePage';
 import JoinPage from './components/JoinPage';
 import Room from './components/Room';
+import Home from "./components/Home";
 
-const routing = (
+const App = (
   <Router>
     <div>
-      {/* Leave this uncommented out code for now, in case we want some form of these later */}
-      {/* <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/rooms">Rooms</Link>
-        </li>
-        <li>
-          <Link to="/join">Join</Link>
-        </li>
-      </ul> */}
-      <Route exact path="/" component={HomePage} />
+      <Route exact path="/" component={Home} />
       <Route path="/rooms" component={RoomsPage} />
       <Route path="/join" component={JoinPage} />
       <Route path="/room" component={Room} />
     </div>
   </Router>
-)
+);
 
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(App, document.getElementById('root'))
