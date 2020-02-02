a0338303c1ff553300938c27c4121e61db520b85
Cowan Cumming <ctcumming@gmail.com>
2020-02-02 01:48:50 -0800

    Able to add videos to queue which show up with title.

--- backend/src/sockets/handler.ts
+++ backend/src/sockets/handler.ts
@@ -1,8 +1,11 @@
 import { Server, Socket } from "socket.io";
 import { Event } from "./event";
+import axios from "axios";
+import logger from "../config/logger";
+import qs from "querystring";
 
 type EventHandler = {
-  [event in Event]?: Function;
+  [event in Event]?: (io: Server, socket: Socket, roomId: string, args: any) => Promise<void>;
 };
 
 const handlers: EventHandler = {};
@@ -12,24 +15,45 @@ const joinRoom = (io: Server, socket: Socket, roomId: string): void => {
     socket.to(roomId).emit(Event.MESSAGE, "A new user has joined the room!");
     for (const [event, handler] of Object.entries(handlers)) {
       if (handler) {
-        socket.on(event, data => {
-          handler(io, socket, roomId, data);
+        socket.on(event, async data => {
+          await handler(io, socket, roomId, data);
         });
       }
     }
   });
 };
 
-handlers[Event.PLAY_VIDEO] = (io: Server, socket: Socket, roomId: string, time: number): void => {
+handlers[Event.PLAY_VIDEO] = (io: Server, socket: Socket, roomId: string, time: number): Promise<void> => {
   socket.to(roomId).emit(Event.PLAY_VIDEO, time);
+  return Promise.resolve();
 };
 
-handlers[Event.PAUSE_VIDEO] = (io: Server, socket: Socket, roomId: string, time: number): void => {
+handlers[Event.PAUSE_VIDEO] = (io: Server, socket: Socket, roomId: string, time: number): Promise<void> => {
   socket.to(roomId).emit(Event.PAUSE_VIDEO, time);
+  return Promise.resolve();
 };
 
-handlers[Event.ADD_TO_QUEUE] = (io: Server, socket: Socket, roomId: string, videoUrl: string): void => {
-  socket.to(roomId).emit(Event.ADD_TO_QUEUE, videoUrl);
+handlers[Event.REQUEST_ADD_TO_QUEUE] = async (
+  io: Server,
+  socket: Socket,
+  roomId: string,
+  videoUrl: string
+): Promise<void> => {
+  logger.info("REQUEST_ADD_TO_QUEUE called!");
+
+  try {
+    // TODO: Improve method of finding video ID to account for extra query parameters
+    const videoId = videoUrl.split("v=")[1];
+
+    const videoInfoResponse = await axios.get(`https://youtube.com/get_video_info?video_id=${videoId}`);
+    const videoInfo = qs.parse(videoInfoResponse.data);
+    const playerResponse = JSON.parse(videoInfo["player_response"] as string);
+    const videoTitle = playerResponse.videoDetails.title;
+    logger.info(`Video found: ${videoTitle}`);
+    io.in(roomId).emit(Event.ADD_TO_QUEUE, videoTitle);
+  } catch {
+    logger.error("Failed to find info about video");
+  }
 };
 
 export default joinRoom;
--- frontend/src/sockets/event.ts
+++ frontend/src/sockets/event.ts
@@ -1,9 +1,10 @@
 export enum Event {
+  ADD_TO_QUEUE = "add to queue",
   CONNECT = "connect",
   DISCONNECT = "disconnect",
   MESSAGE = "message",
   JOIN_ROOM = "join room",
   PLAY_VIDEO = "play video",
   PAUSE_VIDEO = "pause video",
-  ADD_TO_QUEUE = "add to queue"
+  REQUEST_ADD_TO_QUEUE = "request add to queue"
 }
