a6fffee15a51b57f30729b2531af007f884b9d85
Cowan Cumming <ctcumming@gmail.com>
2020-02-02 23:50:32 -0800

    Moved socket.io stuff out of Queue.tsx.

--- frontend/src/components/Queue.tsx
+++ frontend/src/components/Queue.tsx
@@ -1,45 +1,25 @@
 import React from "react";
 import { List, ListItem, ListItemText, TextField, ListItemSecondaryAction, IconButton } from "@material-ui/core";
 import { createStyles, withStyles } from "@material-ui/core/styles";
-import { Event } from "../sockets/event";
 import AddIcon from "@material-ui/icons/Add";
 import Video from "../models/video";
 
 interface Props {
   classes: any;
-  socket: SocketIOClient.Socket;
+  onAddVideo: (url: string) => void;
+  videos: Video[];
 }
 
 interface State {
   newVideoUrl: string;
-  videos: Video[];
 }
 
 class Queue extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      newVideoUrl: "",
-      videos: []
+      newVideoUrl: ""
     };
-
-    this.add = this.add.bind(this);
-    this.requestAdd = this.requestAdd.bind(this);
-  }
-
-  requestAdd(videoUrl: string): void {
-    this.props.socket.emit(Event.REQUEST_ADD_TO_QUEUE, videoUrl);
-  }
-
-  add(video: Video): void {
-    const videos = this.state.videos;
-    videos.push(video);
-
-    this.setState({ videos });
-  }
-
-  componentDidMount() {
-    this.props.socket.on(Event.ADD_TO_QUEUE, (video: Video) => this.add(video));
   }
 
   render() {
@@ -56,12 +36,12 @@ class Queue extends React.Component<Props, State> {
             onChange={event => this.setState({ newVideoUrl: event.target.value })}
           />
           <ListItemSecondaryAction>
-            <IconButton edge="end" aria-label="comments" onClick={() => this.requestAdd(this.state.newVideoUrl)}>
+            <IconButton edge="end" aria-label="comments" onClick={() => this.props.onAddVideo(this.state.newVideoUrl)}>
               <AddIcon style={{ color: "white" }} />
             </IconButton>
           </ListItemSecondaryAction>
         </ListItem>
-        {this.state.videos.map((video, i) => (
+        {this.props.videos.map((video, i) => (
           <ListItem key={i}>
             <ListItemText primary={video.title} />
           </ListItem>
