8ad672079fbaf9b73c93e4d45a3d807ff915337d
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2020-02-02 23:17:39 -0800

    copied over package-lock to chatfeature

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,47 +1,44 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
-import queryString from 'query-string';
-import io from 'socket.io-client';
-import { ReactComponent } from "*.svg";
 
 const styles = {
   chatBox: {
     border: "10px solid white",
-    "borderRadius": "20%",
+    borderRadius: "20%",
     color: "white",
-    "maxWidth": "10vw",
-    height: "50vh",
+    maxWidth: "10vw",
+    height: "50vh"
   }
-}
-
+};
 
 const display = {
   messages: []
-}
+};
 
 type MessageProps = {
-  message: string
-}
+  message: string;
+};
 
-const Message: FunctionComponent<MessageProps> = ({ message }) => <div>
-  <p>{message}</p>
-</div>
+const Message: FunctionComponent<MessageProps> = ({ message }) => (
+  <div>
+    <p>{message}</p>
+  </div>
+);
 
 interface Props {
-  sendMessage: Function,
-  messages: Array<String>,
+  sendMessage: Function;
+  messages: Array<string>;
 }
 
 interface State {
-  message: string
+  message: string;
 }
 
 class Chat extends React.Component<Props, State> {
-
   constructor(props: Props) {
     super(props);
     this.state = {
-      message: "",
-    }
+      message: ""
+    };
     this.onChange = this.onChange.bind(this);
     this.enterPressed = this.enterPressed.bind(this);
   }
@@ -52,22 +49,20 @@ class Chat extends React.Component<Props, State> {
 
   enterPressed(event: React.KeyboardEvent) {
     event.preventDefault();
-    var code = event.keyCode || event.which;
+    const code = event.keyCode || event.which;
     if (code === 13) {
       this.props.sendMessage(this.state.message);
       this.setState({ message: "" });
     }
-
   }
-
-
 
   render() {
     return (
       <div style={styles.chatBox}>
         <h3>Chat:</h3>
         {display.messages}
-        <input placeholder="Send a message"
+        <input
+          placeholder="Send a message"
           name="chatinput"
           onChange={this.onChange}
           onKeyUp={this.enterPressed}
@@ -76,23 +71,6 @@ class Chat extends React.Component<Props, State> {
       </div>
     );
   }
-
 }
 
 export default Chat;
-
-
-// useEffect(() => {
-  //   // var { name, room } = queryString.parse(window.location.search);
-
-  //   let socket = io(ENDPOINT);
-  //   socket.emit('join', data, () => {
-  //     alert("printing again");
-  //   });
-
-  //   return () => {
-  //     socket.emit('disconnect');
-
-  //     socket.off('disconnect');
-  //   }
-  // }, [ENDPOINT, window.location.search]);
