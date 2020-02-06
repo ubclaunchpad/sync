22a6e7fdb62624e587c99ddc0c17af1c87d3e6e4
Cowan Cumming <ctcumming@gmail.com>
2020-02-06 00:13:05 -0800

    Refactored event handler to be an object.

--- backend/src/sockets/handler.ts
+++ backend/src/sockets/handler.ts
@@ -5,64 +5,78 @@ import logger from "../config/logger";
 import qs from "querystring";
 import Video from "../models/video";
 import uniqid from "uniqid";
+import Database from "../core/database";
 
 type EventHandler = {
-  [event in Event]?: (io: Server, socket: Socket, roomId: string, args: any) => Promise<void>;
+  [event in Event]?: (args: any) => Promise<void>;
 };
 
-const handlers: EventHandler = {};
+class RoomSocketHandler {
+  private database: Database;
+  private io: Server;
+  private socket: Socket;
+  private roomId: string;
 
-const joinRoom = (io: Server, socket: Socket, roomId: string): void => {
-  socket.join(roomId, () => {
-    socket.to(roomId).emit(Event.MESSAGE, "A new user has joined the room!");
-    for (const [event, handler] of Object.entries(handlers)) {
-      if (handler) {
-        socket.on(event, async data => {
-          await handler(io, socket, roomId, data);
-        });
-      }
-    }
-  });
-};
-
-handlers[Event.PLAY_VIDEO] = (io: Server, socket: Socket, roomId: string, time: number): Promise<void> => {
-  socket.to(roomId).emit(Event.PLAY_VIDEO, time);
-  return Promise.resolve();
-};
-
-handlers[Event.PAUSE_VIDEO] = (io: Server, socket: Socket, roomId: string, time: number): Promise<void> => {
-  socket.to(roomId).emit(Event.PAUSE_VIDEO, time);
-  return Promise.resolve();
-};
-
-handlers[Event.REQUEST_ADD_TO_QUEUE] = async (
-  io: Server,
-  socket: Socket,
-  roomId: string,
-  videoUrl: string
-): Promise<void> => {
-  logger.info("REQUEST_ADD_TO_QUEUE called!");
-
-  try {
-    // TODO: Improve method of finding video ID to account for extra query parameters
-    const videoId = videoUrl.split("v=")[1];
-
-    const videoInfoResponse = await axios.get(`https://youtube.com/get_video_info?video_id=${videoId}`);
-    const videoInfo = qs.parse(videoInfoResponse.data);
-    const playerResponse = JSON.parse(videoInfo["player_response"] as string);
-    const videoTitle = playerResponse.videoDetails.title;
-    logger.info(`Video found: ${videoTitle}`);
-
-    const video: Video = { id: uniqid(), title: videoTitle, url: videoUrl };
-    io.in(roomId).emit(Event.ADD_TO_QUEUE, video);
-  } catch {
-    logger.error("Failed to find info about video");
+  constructor(database: Database, io: Server, socket: Socket, roomId: string) {
+    this.database = database;
+    this.io = io;
+    this.socket = socket;
+    this.roomId = roomId;
   }
-};
 
-handlers[Event.SET_VIDEO] = (io: Server, socket: Socket, roomId: string, video: Video): Promise<void> => {
-  io.in(roomId).emit(Event.SET_VIDEO, video);
-  return Promise.resolve();
-};
+  initialize(): void {
+    this.socket.join(this.roomId, () => {
+      const handlers = this.createEventHandler();
 
-export default joinRoom;
+      this.socket.to(this.roomId).emit(Event.MESSAGE, "A new user has joined the room!");
+      for (const [event, handler] of Object.entries(handlers)) {
+        if (handler) {
+          this.socket.on(event, async data => {
+            await handler(data);
+          });
+        }
+      }
+    });
+  }
+
+  private createEventHandler(): EventHandler {
+    return {
+      [Event.PLAY_VIDEO]: (time: number): Promise<void> => {
+        this.socket.to(this.roomId).emit(Event.PLAY_VIDEO, time);
+        return Promise.resolve();
+      },
+
+      [Event.PAUSE_VIDEO]: (time: number): Promise<void> => {
+        this.socket.to(this.roomId).emit(Event.PAUSE_VIDEO, time);
+        return Promise.resolve();
+      },
+
+      [Event.REQUEST_ADD_TO_QUEUE]: async (videoUrl: string): Promise<void> => {
+        logger.info("REQUEST_ADD_TO_QUEUE called!");
+
+        try {
+          // TODO: Improve method of finding video ID to account for extra query parameters
+          const videoId = videoUrl.split("v=")[1];
+
+          const videoInfoResponse = await axios.get(`https://youtube.com/get_video_info?video_id=${videoId}`);
+          const videoInfo = qs.parse(videoInfoResponse.data);
+          const playerResponse = JSON.parse(videoInfo["player_response"] as string);
+          const videoTitle = playerResponse.videoDetails.title;
+          logger.info(`Video found: ${videoTitle}`);
+
+          const video: Video = { id: uniqid(), title: videoTitle, url: videoUrl };
+          this.io.in(this.roomId).emit(Event.ADD_TO_QUEUE, video);
+        } catch {
+          logger.error("Failed to find info about video");
+        }
+      },
+
+      [Event.SET_VIDEO]: (video: Video): Promise<void> => {
+        this.io.in(this.roomId).emit(Event.SET_VIDEO, video);
+        return Promise.resolve();
+      }
+    };
+  }
+}
+
+export default RoomSocketHandler;
