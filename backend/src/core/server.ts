0ffc94a9b224fc7e9df27c204103a8b6375dedd7
Cowan Cumming <ctcumming@gmail.com>
2020-02-06 00:13:05 -0800

    Refactored event handler to be an object.

--- backend/src/core/server.ts
+++ backend/src/core/server.ts
@@ -7,16 +7,19 @@ import API from "./api";
 import { Event } from "../sockets/event";
 import joinRoom from "../sockets/handler";
 import logger from "../config/logger";
+import RoomSocketHandler from "../sockets/handler";
 
 export default class Server {
   private app: express.Application;
   private port: number;
   private httpServer: http.Server;
+  private database: Database;
 
   constructor(port?: number) {
     this.port = port || 8080;
     this.app = express();
     this.httpServer = http.createServer(this.app);
+    this.database = new Database(6379, process.env.DB_HOST);
     this.app.use((req, res, next) => {
       res.header("Access-Control-Allow-Origin", "http://localhost:3000");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -24,7 +27,7 @@ export default class Server {
     });
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.use("/", new API(new Database(6379, process.env.DB_HOST)).router);
+    this.app.use("/", new API(this.database).router);
     this.setupSockets();
   }
 
@@ -40,7 +43,7 @@ export default class Server {
       logger.debug(`Socket ${socket.id} connected.`);
 
       socket.on(Event.JOIN_ROOM, roomId => {
-        joinRoom(io, socket, roomId);
+        new RoomSocketHandler(this.database, io, socket, roomId).initialize();
       });
 
       socket.on(Event.DISCONNECT, socket => {
