158f278ec9aa40c4ea1b0f7918aea4c1fc4635f5
John Aguilar <42220961+johnagl@users.noreply.github.com>
2020-02-26 21:10:14 -0800

    Refactor

--- frontend/src/components/VideoChat.tsx
+++ frontend/src/components/VideoChat.tsx
@@ -8,6 +8,9 @@ interface VideoChatProps {
 interface VideoChatState {
   gotAnswer: boolean,
   stream: any,
+  peer: any,
+  peerVideo: any,
+  startVideo: boolean,
 }
 
 interface PeerType {
@@ -36,24 +39,19 @@ class VideoChat extends React.Component<VideoChatProps, VideoChatState> {
     this.state = {
       gotAnswer: false,
       stream: null,
+      peer: null,
+      peerVideo: null,
+      startVideo: false,
     };
     this.videoRef = React.createRef();
     this.peerVideoRef = React.createRef();
-    // this.createVideo = this.createVideo.bind(this);
-    // this.createVideoChat = this.createVideoChat.bind(this);
-    // this.createVideo = this.createVideo.bind(this);
-    // this.makePeer = this.makePeer.bind(this);
-    // this.init = this.init.bind(this);
+    this.createVideo = this.createVideo.bind(this);
+    this.createVideo = this.createVideo.bind(this);
+    this.makePeer = this.makePeer.bind(this);
+    this.init = this.init.bind(this);
   }
 
   componentDidMount() {
-    this.createVideoChat();
-
-  }
-
-  createVideoChat = () => {
-    let client: Client = { gotAnswer: false, peer: null }
-    let socket = this.socket;
     navigator.mediaDevices.getUserMedia({ video: true, audio: true }) // this asks browser for permission to access cam/aud
       .then((stream) => {
         this.setState({ stream: stream });
@@ -62,72 +60,80 @@ class VideoChat extends React.Component<VideoChatProps, VideoChatState> {
         if (node) {
           node.srcObject = stream;
         }
-
-        //Used to initialize a peer, define a new peer and return it
-        const init = (type: any) => {//type tells us if init == true means sends offer, if init is false, wont send offer, wait for offer, send answer
-          let peer = new Peer({ initiator: (type === PeerTypes.init) ? true : false, stream: this.state.stream && this.state.stream, trickle: false });
-          peer.on('stream', function (stream) {//when we get stream from other user, create new video
-            createVideo(stream)
-          });
-          peer.on('close', function () {//when peer is closed, destroy video
-            peer.destroy();//Everything is cleaned up,
-          });
-          return peer;//Return our peer
-        }
-
-
-        //for peer of type init
-        const makePeer = () => {
-          console.log('makePeer called');
-          let peer = init('init');
-          peer.on('signal', function (data) {
-            if (!client.gotAnswer) {
-              socket.emit('Offer', data);
-            }
-          })
-          client.peer = peer;
-        }
-
-        // Used when we get an offer from another client, we want to send him the answer
-        // We'll have to define the peer, but it wont be of type notInit
-        // for peer type of notInit
-        const frontAnswer = (offer: any) => {
-          let peer = init('notInit');
-          //this doesnt run automatically, have to call signal
-          peer.on('signal', (data) => {
-            //means we have gotten our offer
-            //we want to imitate with even Answer
-            socket.emit('Answer', data);
-          })
-          //pass offer to signal, generate answer to backend, which will send to other user
-          peer.signal(offer)
-        }
-
-        // handles when answer comes from backend
-        // if answer comes from backend, set the client.gotAnswer to true, and signalAnswerView to peer, 
-        // then finally both the clients will be connected
-        const signalAnswer = (answer: any) => {
-          client.gotAnswer = true;
-          let peer = client.peer;
-          peer.signal(answer)
-        }
-
-
-        const createVideo = (stream: any) => {
-          const peerNode = this.peerVideoRef.current;
-          if (peerNode) {
-            peerNode.srcObject = stream
-          }
-        }
-
-        this.socket.on('BackOffer', frontAnswer);
-        this.socket.on('BackAnswer', signalAnswer); //if answer is from backend, handle, connect both clients
-        this.socket.on('SessionActive', () => console.log('Session Active'));
-        this.socket.on('CreatePeer', () => {
-          makePeer();
-        });
       })
-      .catch();//If permission not given, display error 
+      .catch();//If permission not given, display error
+
+    this.socket.on('BackOffer', this.frontAnswer);
+    this.socket.on('BackAnswer', this.signalAnswer); //if answer is from backend, handle, connect both clients
+    this.socket.on('SessionActive', () => console.log('Session Active'));
+    this.socket.on('CreatePeer', this.makePeer);
+
+  }
+
+  //Used to initialize a peer, define a new peer and return it
+  init = (type: any): Peer.Instance => {//type tells us if init == true means sends offer, if init is false, wont send offer, wait for offer, send answer
+    let socket = this.socket;
+    let peer = new Peer({ initiator: (type === PeerTypes.init) ? true : false, stream: this.state.stream && this.state.stream, trickle: false });
+
+    let peerNode = this.peerVideoRef.current;
+    peer.on('stream', function (stream) {//when we get stream from other user, create new video
+      // // createVideo(stream)
+      // const peerNode = this.peerVideoRef.current;
+      if (peerNode) {
+        peerNode.srcObject = stream
+      }
+    });
+    peer.on('close', function () {//when peer is closed, destroy video
+      socket.emit('close');
+      peer.destroy();//Everything is cleaned up,
+    });
+    return peer;//Return our peer
+  }
+
+
+  //for peer of type init
+  makePeer = (): void => {
+    const gotAnswer = this.state.gotAnswer;
+    let socket = this.socket;
+    let peer = this.init('init');
+    peer.on('signal', function (data) {
+      if (!gotAnswer) {
+        socket.emit('Offer', data);
+      }
+    })
+    this.setState({ peer: peer })
+  }
+
+  frontAnswer = (offer: any): void => {
+    let peer = this.init('notInit');
+    let socket = this.socket;
+    //this doesnt run automatically, have to call signal
+    peer.on('signal', (data) => {
+      //means we have gotten our offer
+      //we want to imitate with even Answer
+      socket.emit('Answer', data);
+    })
+    //pass offer to signal, generate answer to backend, which will send to other user
+    peer.signal(offer);
+  }
+
+  // handles when answer comes from backend
+  // if answer comes from backend, set the client.gotAnswer to true, and signalAnswerView to peer, 
+  // then finally both the clients will be connected
+  signalAnswer = (answer: any): void => {
+    this.setState({ gotAnswer: true })
+    // client.gotAnswer = true;
+    // let peer = client.peer;
+    let peer = this.state.peer;
+    peer.signal(answer);
+  }
+
+  createVideo = (stream: any): void => {
+    const peerNode = this.peerVideoRef.current;
+    if (peerNode) {
+      peerNode.srcObject = stream
+      this.setState({ peerVideo: stream });
+    }
   }
 
   render() {
@@ -135,7 +141,9 @@ class VideoChat extends React.Component<VideoChatProps, VideoChatState> {
       <React.Fragment>
         <h1>Test Video Chat</h1>
         <video ref={this.videoRef} autoPlay></video>
-        <video ref={this.peerVideoRef} autoPlay></video>
+        {<video ref={this.peerVideoRef} autoPlay></video>}
+        <button onClick={() => this.setState({ startVideo: true })}>Start</button>
+        <button onClick={() => this.setState({ startVideo: false })}>End</button>
       </React.Fragment>
     );
   }
