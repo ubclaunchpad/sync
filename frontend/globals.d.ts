881347e5514c2d56520c611aecac5e0c944aaaa3
klynx <KevinLynx_L@hotmail.com>
2020-02-29 14:23:06 -0800

    Added in youtube title

---
+++ frontend/globals.d.ts
@@ -0,0 +1,1 @@
+declare module "get-youtube-title";
--- frontend/src/components/Browse.tsx
+++ frontend/src/components/Browse.tsx
@@ -10,45 +10,62 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Grow from "@material-ui/core/Grow";
 
+import getYoutubeTitle from "get-youtube-title";
+
 interface Props {
   classes: any;
 }
 
 interface State {
   roomList: any;
+  vidTitle: any;
 }
 
 export class Browse extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      roomList: []
+      roomList: [],
+      vidTitle: {}
     };
+    this.renderRoom = this.renderRoom.bind(this);
   }
 
   async componentDidMount() {
     try {
       const res = await axios.get("http://localhost:8080/rooms");
-      this.setState({
-        roomList: res.data
-      });
+      const numrooms = Object.keys(res.data).length;
+      let numParsed = 0;
+      for (const key in res.data) {
+        getYoutubeTitle(res.data[key].currVideoId, (err: any, title: any) => {
+          this.state.vidTitle[res.data[key].currVideoId] = title;
+          numParsed++;
+          if (numParsed === numrooms) {
+            this.setState({
+              roomList: res.data
+            });
+          }
+        });
+      }
     } catch (err) {
       console.log(err);
     }
   }
 
   renderRoom(room: any, key: any) {
+    console.log(this.state);
     const youtubeId = key.split(":")[1];
     return (
       <Grow in={true} timeout="auto" key={key}>
         <TableRow hover key={key}>
           <TableCell>
             <img
+              alt={key}
               src={`https://img.youtube.com/vi/${room.currVideoId}/default.jpg`}
               onClick={event => (window.location.href = "/rooms/" + youtubeId)}
             ></img>
           </TableCell>
-          <TableCell>{room.currVideoId}</TableCell>
+          <TableCell>{this.state["vidTitle"][room.currVideoId]}</TableCell>
           <TableCell>{room.name}</TableCell>
           <TableCell>{key}</TableCell>
           <TableCell>
