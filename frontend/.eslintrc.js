e63bfeb49d52d837eb745eb0172fd0c062c44faa
Srijon Saha <srijon365@gmail.com>
2020-03-04 20:15:24 -0800

    Remove old package and remove bad rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,8 +17,7 @@ module.exports = {
     "no-var": "error",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-empty-interface": "off"
+    "@typescript-eslint/explicit-function-return-type": "off"
   },
   settings:  {
     react:  {
