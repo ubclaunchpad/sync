957183c171fcf8e2b5b43b8c47c5fc8a213a5580
Cowan Cumming <ctcumming@gmail.com>
2020-03-07 14:23:20 -0800

    Put logs back to info.

--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -1,7 +1,7 @@
 import pino from "pino";
 
 const logger = pino({
-  level: process.env.LOG_LEVEL || "debug",
+  level: process.env.LOG_LEVEL || "info",
   base: null,
   prettyPrint: true,
   timestamp: () => {
