9afe829f59aa93442448baaea3d114b748310c95
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2020-03-07 10:59:55 -0800

    added basic interface for chat

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,5 +1,10 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
-
+import React, { FunctionComponent, useState, useEffect, ChangeEvent } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 const styles = {
   chatBox: {
     border: "1px solid white",
@@ -10,28 +15,31 @@ const styles = {
   }
 };
 
+interface Message {
+  user: string;
+  message: string;
+}
+
 interface Props {
   sendMessage: Function;
-  signIn: Function;
+  messages: Message[];
 }
 
 interface State {
   message: string;
-  signedIn: boolean;
 }
 
 class Chat extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      message: "",
-      signedIn: false
+      message: ""
     };
     this.onChange = this.onChange.bind(this);
     this.enterPressed = this.enterPressed.bind(this);
   }
 
-  onChange = (event: React.FormEvent<HTMLInputElement>) => {
+  onChange = (event: ChangeEvent<HTMLInputElement>) => {
     this.setState({ message: event.currentTarget.value });
   };
 
@@ -39,33 +47,29 @@ class Chat extends React.Component<Props, State> {
     event.preventDefault();
     const code = event.keyCode || event.which;
     if (code === 13) {
-      if (this.state.signedIn) {
-        this.props.sendMessage(this.state.message);
-      } else {
-        this.props.signIn(this.state.message);
-        if (this.state.message) {
-          this.setState({ signedIn: true });
-        }
-      }
+      this.props.sendMessage(this.state.message);
       this.setState({ message: "" });
     }
   };
 
+  renderChat = () => {
+    const chat = [];
+    for (const m of this.props.messages) {
+      chat.push(
+        <span>
+          <ListItemText primary={m.user + ": " + m.message} />
+          <Divider variant="middle" />
+        </span>
+      );
+    }
+    return chat;
+  };
+
   render() {
-    const signInWindow = (
-      <div>
-        <input
-          placeholder="What is your name?"
-          onChange={this.onChange}
-          onKeyUp={this.enterPressed}
-          value={this.state.message}
-        />
-      </div>
-    );
     const chatWindow = (
       <div>
-        <input
-          placeholder="Send a message"
+        <TextField
+          placeholder="Send a Message!"
           onChange={this.onChange}
           onKeyUp={this.enterPressed}
           value={this.state.message}
@@ -74,8 +78,10 @@ class Chat extends React.Component<Props, State> {
     );
     return (
       <div style={styles.chatBox}>
-        <h3>Chat:</h3>
-        {this.state.signedIn ? chatWindow : signInWindow}
+        {chatWindow}
+        <Paper style={{ maxHeight: "30vh", overflow: "auto" }}>
+          <List aria-label="ChatRoom">{this.renderChat()}</List>
+        </Paper>
       </div>
     );
   }
