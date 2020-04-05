1efa341a0d207a5c4817bd1740333e6e83d9d6d0
klynx <Kevinlynx_L@hotmail.com>
2020-04-05 05:15:19 -0700

    Cleaned up code

--- frontend/src/components/Browse.tsx
+++ frontend/src/components/Browse.tsx
@@ -1,28 +1,10 @@
 import React from "react";
 import axios from "axios";
 
-// import Button from "@material-ui/core/Button";
-// import Table from "@material-ui/core/Table";
-// import TableBody from "@material-ui/core/TableBody";
-// import TableCell from "@material-ui/core/TableCell";
-// import TableHead from "@material-ui/core/TableHead";
-// import TableRow from "@material-ui/core/TableRow";
-// import Paper from "@material-ui/core/Paper";
 import Grow from "@material-ui/core/Grow";
-
-import { createStyles, withStyles } from "@material-ui/core";
-// import Card from "@material-ui/core/Card";
-// import CardActionArea from "@material-ui/core/CardActionArea";
-// import CardActions from "@material-ui/core/CardActions";
-// import CardContent from "@material-ui/core/CardContent";
-// import CardMedia from "@material-ui/core/CardMedia";
-// // import Button from '@material-ui/core/Button';
-// import Typography from "@material-ui/core/Typography";
-
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
-import ListSubheader from "@material-ui/core/ListSubheader";
 import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
 import logo from "../images/logo.png";
@@ -39,29 +21,6 @@ interface State {
   width: any;
   height: any;
 }
-
-const materialUiStyles = createStyles({
-  // root: {
-  //   maxWidth: 345
-  // },
-  // media: {
-  //   height: 140
-  // },
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden"
-    // backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 500,
-    height: 450
-  },
-  icon: {
-    color: "rgba(255, 255, 255, 0.54)"
-  }
-});
 
 export class Browse extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -96,19 +55,12 @@ export class Browse extends React.Component<Props, State> {
     return (
       <Grow in={true} timeout="auto" key={key}>
         <GridListTile
+          className="tile"
           key={`https://img.youtube.com/vi/${room.currVideoId}/default.jpg`}
           onClick={event => (window.location.href = "/rooms/" + roomId)}
         >
           <img src={`https://img.youtube.com/vi/${room.currVideoId}/default.jpg`} alt={room.name} />
-          <GridListTileBar
-            title={room.name}
-            subtitle={<span>Room ID: {roomId}</span>}
-            actionIcon={
-              <IconButton aria-label={`info about ${room.name}`} className={classes.icon}>
-                <InfoIcon />
-              </IconButton>
-            }
-          />
+          <GridListTileBar title={room.name} subtitle={<span>Room ID: {roomId}</span>} />
         </GridListTile>
       </Grow>
     );
@@ -133,4 +85,4 @@ export class Browse extends React.Component<Props, State> {
   }
 }
 
-export default withStyles(materialUiStyles)(Browse);
+export default Browse;
