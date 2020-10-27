785b7df1c25adb82195b547a91556f21cec1acbe
Srijon Saha <srijon365@gmail.com>
2020-10-27 14:42:28 -0700

    Update order of social media buttons

--- frontend/src/components/Browse.tsx
+++ frontend/src/components/Browse.tsx
@@ -109,14 +109,14 @@ export class Browse extends React.Component<Props, State> {
                   <Facebook style={{ color: "rgb(33, 120, 232)" }}></Facebook>
                 </IconButton>
               </Tooltip>
-              <Tooltip title="Share to Reddit">
-                <IconButton target="_blank" href={redditURL}>
-                  <Reddit style={{ color: "rgb(246, 69, 29)" }}></Reddit>
-                </IconButton>
-              </Tooltip>
               <Tooltip title="Share to Twitter">
                 <IconButton target="_blank" href={twitterURL}>
                   <Twitter style={{ color: "rgb(41, 159, 232)" }}></Twitter>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Share to Reddit">
+                <IconButton target="_blank" href={redditURL}>
+                  <Reddit style={{ color: "rgb(246, 69, 29)" }}></Reddit>
                 </IconButton>
               </Tooltip>
             </div>
